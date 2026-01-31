1b64e5a357bec1ae8325b506fb79e880d8b929e8
dipankar2002 <dipankarganguly15@gmail.com>
2026-01-31 19:39:01 +0000

    assignment done

--- 01-async-js/medium/1-file-cleaner.js
+++ 01-async-js/medium/1-file-cleaner.js
@@ -11,3 +11,19 @@
 // ```
 // hello world my name is raman
 // ```
+
+const fs = require("fs");
+
+// this will store the file content into string type.
+const data = fs.readFileSync("./medium/text_file.txt", "utf-8");
+
+// const newString = data.replace(/\s+/g, " ").trim(); // this is only remove spaces and lines into one line.
+
+// this will remove all spaces and extra lines.
+const newString = data
+.split("\n") // divide every line into array.
+.map(line => line.replace(/\s+/g, " ").trim()) // remove inside spaces from every array string.
+.filter(line => line.length>0) // remove all empty string array elements and reduce lines.
+.join("\n"); // join the string from the array of strings.
+
+fs.writeFileSync("./medium/text_file.txt", newString);
